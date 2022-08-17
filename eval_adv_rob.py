b48294dca7f50deaca4db7a36aa008ebce59b18b
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2022-08-17 09:40:29 +0200

    eval: adding more options for adv evaluation

--- eval_adv_rob.py
+++ eval_adv_rob.py
@@ -1,3 +1,4 @@
+from ctypes import LibraryLoader
 import torch
 import sys
 import argparse
@@ -5,7 +6,8 @@ from train import Trainer
 import shutil
 from autoattack import AutoAttack
 from utils import NormalizedModel
-
+import os
+import data
 
 
 def main(args):
@@ -22,12 +24,29 @@ def main(args):
     
     all_x = []
     all_y = []
-    for x, y in trainer.dataset.val_dataloader(): 
-        all_x.append(x)
-        all_y.append(y)
+    loader = None
 
-    all_x = torch.vstack(all_x).to(trainer.device)
-    all_y = torch.hstack(all_y).to(trainer.device)
+    loader_batch = args.n_samples
+    if args.n_samples == -1:
+        loader_batch = saved_args.batch_size
+
+    trainer.dataset = data.get_dataset(saved_args.dataset)(os.path.join(
+            saved_args.dataset_dir, saved_args.dataset), loader_batch, saved_args.num_workers)
+
+    if args.data_split == "val":
+        loader = trainer.dataset.val_dataloader()
+    elif args.data_split == "train":
+        loader = trainer.dataset.train_dataloader()
+
+    for x, y in loader: 
+        all_x.append(x.to(trainer.device))
+        all_y.append(y.to(trainer.device))
+
+        if args.n_samples == -1:
+            break
+
+    all_x = torch.vstack(all_x)
+    all_y = torch.hstack(all_y)
 
     model = NormalizedModel(trainer.model, trainer.dataset.mean, trainer.dataset.std).to(trainer.device)
     
@@ -46,6 +65,8 @@ if __name__ == "__main__":
     parser.add_argument("--device", type=str, default="cuda:0")
     parser.add_argument("--norm", type=str, default="Linf")
     parser.add_argument("--eps", type=float, default=8/255)
+    parser.add_argument("--data_split", type=str, default="val", choices=["train", "val"])
+    parser.add_argument("--n_samples", type=int, default=-1)
     _args = parser.parse_args()
     main(_args)
     sys.exit(0)
