f654578119b3e37b6e1c07e741cd17656b7d712a
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2022-08-21 16:59:00 +0000

    eval_adb_rob: adjustign to new trainer

--- eval_adv_rob.py
+++ eval_adv_rob.py
@@ -3,7 +3,6 @@ import torch
 import sys
 import argparse
 from train import Trainer
-import shutil
 from autoattack import AutoAttack
 from utils import NormalizedModel
 import os
@@ -19,26 +18,29 @@ def main(args):
 
     vars(saved_args)["load_checkpoint"] = args.load_checkpoint
     vars(saved_args)["device"] = args.device
-    
-    trainer = Trainer(saved_args, ".temp")
-    
-    all_x = []
-    all_y = []
-    loader = None
 
     loader_batch = args.n_samples
     if args.n_samples == -1:
         loader_batch = saved_args.batch_size
 
-    trainer.dataset = data.get_dataset(saved_args.dataset)(os.path.join(
+    dataset = data.get_dataset(saved_args.dataset)(os.path.join(
             saved_args.dataset_dir, saved_args.dataset), loader_batch, saved_args.num_workers)
 
-    if args.data_split == "val":
-        loader = trainer.dataset.val_dataloader()
-    elif args.data_split == "train":
-        loader = trainer.dataset.train_dataloader()
+    vars(saved_args)["model_in_channels"] = dataset.in_channels
+    vars(saved_args)["model_num_classes"] = dataset.num_classes
 
-    for x, y in loader: 
+    trainer = Trainer(saved_args)
+
+    all_x = []
+    all_y = []
+    loader = None
+
+    if args.data_split == "val":
+        loader = dataset.val_dataloader()
+    elif args.data_split == "train":
+        loader = dataset.train_dataloader()
+
+    for x, y in loader:
         all_x.append(x.to(trainer.device))
         all_y.append(y.to(trainer.device))
 
@@ -48,15 +50,13 @@ def main(args):
     all_x = torch.vstack(all_x)
     all_y = torch.hstack(all_y)
 
-    model = NormalizedModel(trainer.model, trainer.dataset.mean, trainer.dataset.std).to(trainer.device)
-    
+    model = NormalizedModel(trainer.model, dataset.mean, dataset.std).to(trainer.device)
+
     all_x = all_x * model.std + model.mean  # unnormalize samples for AA
 
     adversary = AutoAttack(model, norm=args.norm, eps=args.eps)
 
     _ = adversary.run_standard_evaluation(all_x, all_y)
-
-    shutil.rmtree(".temp")
 
 
 if __name__ == "__main__":
