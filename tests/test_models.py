2a125b7268218390137ded510bb7c88fd6daa040
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2022-10-19 15:00:07 +0200

    tests: updating suite
    - removing activation test for now
    - extending model test by processing a small dummy batch

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,28 +1,31 @@
 import models
 import torch.nn as nn
+import torch
 
 
 def test_model_init():
+    x = torch.random(1, 3, 32, 32)
     for model_name in models.all_classifiers:
         print(model_name)
         model = models.get_model(model_name)(in_channels=3, num_classes=10)
+        model(x)
         assert model is not None
 
 
-def test_model_init_with_activation():
+# def test_model_init_with_activation():
 
-    activation_fn = nn.ELU
+#     activation_fn = nn.ELU
 
-    for model_name in  models.all_classifiers:
-        print(model_name)
-        model = models.get_model(model_name)(in_channels=3, num_classes=10, activation_fn=activation_fn)
+#     for model_name in  models.all_classifiers:
+#         print(model_name)
+#         model = models.get_model(model_name)(in_channels=3, num_classes=10, activation_fn=activation_fn)
         
-        assert model is not None
+#         assert model is not None
 
-        contains_act = False
-        for module in model.modules():
-            assert type(module) is not nn.ReLU
-            if type(module) == activation_fn:
-                contains_act = True
+#         contains_act = False
+#         for module in model.modules():
+#             assert type(module) is not nn.ReLU
+#             if type(module) == activation_fn:
+#                 contains_act = True
                 
-        assert contains_act
+#         assert contains_act
