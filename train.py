1a4b43450665dc320b352c22aa3cd3a984c5c50b
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2022-10-21 10:09:45 +0200

    train: adding better options to freeze sepcific convs

--- train.py
+++ train.py
@@ -73,16 +73,21 @@ class Trainer:
                     except:
                         logging.info("... failed")
 
-        if get_arg(args, "freeze_layers"):
+        if get_arg(args, "freeze_layers") == "conv3x3":
             for mname, module in filter(lambda t: len(list(t[1].children())) == 0, model.named_modules()):
-                if type(module).__name__ in args.freeze_layers.split(","):
+                if type(module) is torch.nn.Conv2d and module.kernel_size == (3, 3):
                     for pname, param in module.named_parameters():
                         logging.info(f"Freezing {mname}/{pname} {module}")
                         param.requires_grad = False
-
-        if get_arg(args, "freeze_conv2d_3x3"):
+        elif get_arg(args, "freeze_layers") == "conv1x1":
             for mname, module in filter(lambda t: len(list(t[1].children())) == 0, model.named_modules()):
-                if type(module) is torch.nn.Conv2d and module.kernel_size != (1, 1):
+                if type(module) is torch.nn.Conv2d and module.kernel_size == (1, 1):
+                    for pname, param in module.named_parameters():
+                        logging.info(f"Freezing {mname}/{pname} {module}")
+                        param.requires_grad = False
+        elif get_arg(args, "freeze_layers"):
+            for mname, module in filter(lambda t: len(list(t[1].children())) == 0, model.named_modules()):
+                if type(module).__name__ in args.freeze_layers.split(","):
                     for pname, param in module.named_parameters():
                         logging.info(f"Freezing {mname}/{pname} {module}")
                         param.requires_grad = False
