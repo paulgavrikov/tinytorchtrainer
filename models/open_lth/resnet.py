c6cda011f4725eb0c7555a3759c2167383b7a20c
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2023-02-13 16:04:11 +0100

    models:open_lth: param setting via model name

--- models/open_lth/resnet.py
+++ models/open_lth/resnet.py
@@ -4,11 +4,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from re import S
-from statistics import mode
 import torch.nn as nn
-import torch.nn.functional as F
 from functools import partial
+import re
 
 class ResNet(nn.Module):
     """A residual neural network as originally designed for CIFAR-10."""
@@ -17,39 +15,32 @@ class ResNet(nn.Module):
         """A ResNet block."""
 
         @staticmethod
-        def make_conv(f_in: int, f_out: int, stride: int, depthwise: bool=False, spatial: bool=False):
-            if not spatial and not depthwise:
-                return nn.Conv2d(f_in, f_out, kernel_size=3, stride=stride, padding=1, bias=False)
-            elif not spatial and depthwise:
+        def make_conv(f_in: int, f_out: int, stride: int, conv_type: str, kernel_size:int=3, force_no_padding: bool=False, padding_mode: str="zeros"):
+            assert padding_mode in ["zeros", "reflect", "replicate", "circular"], f"Invalid padding mode {padding_mode}."
+
+            padding = (kernel_size // 2)
+            if force_no_padding:
+                padding = 0
+            
+            if not conv_type:
+                return nn.Conv2d(f_in, f_out, kernel_size=kernel_size, stride=stride, padding=padding, padding_mode=padding_mode, bias=False)
+            elif conv_type == "depthwise_separable":
                 return nn.Sequential(
-                    nn.Conv2d(f_in, f_in, kernel_size=3, stride=stride, padding=1, bias=False, groups=f_in),
-                    nn.BatchNorm2d(f_in),
+                    nn.Conv2d(f_in, f_in, kernel_size=kernel_size, stride=stride, padding=padding, padding_mode=padding_mode, bias=False, groups=f_in),
                     nn.Conv2d(f_in, f_out, kernel_size=1, bias=False),
-                )
-            elif spatial and depthwise:
-                return nn.Sequential(
-                    nn.Conv2d(f_in, f_in, kernel_size=(1, 3), stride=(1, stride), padding=(0, 1), bias=False, groups=f_in),
-                    nn.Conv2d(f_in, f_in, kernel_size=(3, 1), stride=(stride, 1), padding=(1, 0), bias=False, groups=f_in),
-                    nn.BatchNorm2d(f_in),
-                    nn.Conv2d(f_in, f_out, kernel_size=1, bias=False),
-                )
-            elif spatial and not depthwise:
-                return nn.Sequential(
-                    nn.Conv2d(f_in, f_out, kernel_size=(1, 3), stride=(1, stride), padding=(0, 1), bias=False),
-                    nn.Conv2d(f_out, f_out, kernel_size=(3, 1), stride=(stride, 1), padding=(1, 0), bias=False),
                 )
             else:
-                raise ValueError("Invalid combination of depthwise and spatial.")
+                raise ValueError(f"Invalid conv_type {conv_type}.")
 
 
-        def __init__(self, f_in: int, f_out: int, activation_fn, downsample=False, depthwise=False, spatial=False):
+        def __init__(self, f_in: int, f_out: int, activation_fn, downsample=False, **conv_args):
             super().__init__()
 
             stride = 2 if downsample else 1
             
-            self.conv1 = ResNet.Block.make_conv(f_in, f_out, stride, depthwise, spatial)
+            self.conv1 = ResNet.Block.make_conv(f_in, f_out, stride, **conv_args)
             self.bn1 = nn.BatchNorm2d(f_out)
-            self.conv2 = ResNet.Block.make_conv(f_out, f_out, 1, depthwise, spatial)
+            self.conv2 = ResNet.Block.make_conv(f_out, f_out, 1, **conv_args)
             self.bn2 = nn.BatchNorm2d(f_out)
 
             self.activation = activation_fn()
@@ -75,7 +66,7 @@ class ResNet(nn.Module):
             out += self.shortcut(x)
             return self.activation(out)
 
-    def __init__(self, plan, spatial=False, depthwise=False, in_channels=3, num_classes=10, activation_fn=None, **kwargs):
+    def __init__(self, plan, in_channels=3, num_classes=10, activation_fn=None, **conv_args):
         super().__init__()
 
         if activation_fn is None:
@@ -84,7 +75,7 @@ class ResNet(nn.Module):
         # Initial convolution.
         current_filters = plan[0][0]
 
-        self.conv = ResNet.Block.make_conv(in_channels, current_filters, stride=1, depthwise=depthwise, spatial=spatial)
+        self.conv = ResNet.Block.make_conv(in_channels, current_filters, stride=1, **conv_args)
         self.bn = nn.BatchNorm2d(current_filters)
         self.activation = activation_fn()
 
@@ -93,7 +84,7 @@ class ResNet(nn.Module):
         for segment_index, (filters, num_blocks) in enumerate(plan):
             for block_index in range(num_blocks):
                 downsample = segment_index > 0 and block_index == 0
-                blocks.append(ResNet.Block(current_filters, filters, activation_fn=activation_fn, downsample=downsample, spatial=spatial, depthwise=depthwise))
+                blocks.append(ResNet.Block(current_filters, filters, activation_fn=activation_fn, downsample=downsample, **conv_args))
                 current_filters = filters
 
         self.blocks = nn.Sequential(*blocks)
@@ -115,67 +106,53 @@ class ResNet(nn.Module):
 
     @property
     def output_layer_names(self):
-        return ['fc.weight', 'fc.bias']
+        return ["fc.weight", "fc.bias"]
 
     @staticmethod
     def get_model_from_name(name, **kwargs):
-        """The naming scheme for a ResNet is 'cifar_resnet_N[_W]'.
+        use_1x1_stem = False
+        kernel_size = 3
+        conv_type = None
+        padding_mode = "zeros"
+        force_no_padding = False
 
-        The ResNet is structured as an initial convolutional layer followed by three "segments"
-        and a linear output layer. Each segment consists of D blocks. Each block is two
-        convolutional layers surrounded by a residual connection. Each layer in the first segment
-        has W filters, each layer in the second segment has 32W filters, and each layer in the
-        third segment has 64W filters.
-
-        The name of a ResNet is 'cifar_resnet_N[_W]', where W is as described above.
-        N is the total number of layers in the network: 2 + 6D.
-        The default value of W is 16 if it isn't provided.
-
-        For example, ResNet-20 has 20 layers. Exclusing the first convolutional layer and the final
-        linear layer, there are 18 convolutional layers in the blocks. That means there are nine
-        blocks, meaning there are three blocks per segment. Hence, D = 3.
-        The name of the network would be 'cifar_resnet_20' or 'cifar_resnet_20_16'.
-        """
-
-        depthwise = False
-        skip_spatial_convs = False
-        spatial = False
         if "_dw" in name:
-            depthwise = True
+            conv_type = "depthwise_separable"
             name = name.replace("_dw", "")
+          
+        if "_1x1stem" in name:
+            use_1x1_stem = True
+            name = name.replace("_1x1stem", "")
+            
+        exp_match = re.search(r"_k[0-9]+", name)
+        if exp_match:
+            kernel_size = int(exp_match.group(0)[2:])
+            name = name.replace(exp_match.group(0), "")
 
-        if "_di" in name:
-            depthwise = True
-            skip_spatial_convs = True
-            name = name.replace("_di", "")
+        exp_match = re.search(r"_padding(.*)", name)
+        if exp_match:
+            padding_mode = exp_match.group(1)
+            name = name.replace(exp_match.group(0), "")
 
-        spatial = False
-        if "_sp" in name:
-            spatial = True
-            name = name.replace("_sp", "")
+        if "_nopad" in name:
+            force_no_padding = True
+            name = name.replace("_nopad", "")
 
-        name = name.split('_')
+        name = name.split("_")
+        
+        assert len(name) <= 3, f"Extra args not understood {name}"
 
         W = 16 if len(name) == 2 else int(name[2])
         D = int(name[1])
         if (D - 2) % 3 != 0:
-            raise ValueError('Invalid ResNet depth: {}'.format(D))
+            raise ValueError("Invalid ResNet depth: {}".format(D))
         D = (D - 2) // 6
         plan = [(W, D), (2*W, D), (4*W, D)]
 
-        resnet = ResNet(plan, spatial=spatial, depthwise=depthwise, **kwargs)
+        resnet = ResNet(plan, conv_type=conv_type, kernel_size=kernel_size, padding_mode=padding_mode, force_no_padding=force_no_padding, **kwargs)
 
-        if skip_spatial_convs:
-            replace_conv2d(resnet, nn.Sequential, filter_cb=lambda m: m.kernel_size != (1, 1) and m.stride == (1, 1))
-            replace_conv2d(resnet, partial(nn.Upsample, scale_factor=0.5, mode="bilinear"), filter_cb=lambda m: m.kernel_size != (1, 1) and m.stride == (2, 2))
+        # must be first, before any other replace!
+        if use_1x1_stem:
+            resnet.conv = nn.Conv2d(in_channels=resnet.conv.in_channels, out_channels=resnet.conv.out_channels, kernel_size=1, bias=False)
+
         return resnet
-
-def replace_conv2d(module, new_cls, filter_cb=None, **kwargs):
-    for name, child in module.named_children():
-   
-        if type(child) == nn.Conv2d and (not filter_cb or (filter_cb and filter_cb(child))) and name != "conv":
-            new_module = new_cls()
-            setattr(module, name, new_module)
-
-    for child in module.children():
-        replace_conv2d(child, new_cls, filter_cb, **kwargs)
